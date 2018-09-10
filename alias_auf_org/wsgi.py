50265e4b3e645b21f31dff48bdb451869ae84575
lenenba <bilitikjulesroger@yahoo.fr>
2018-09-10 16:34:38 -0400

    creation du projet django

---
+++ alias_auf_org/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for alias_auf_org project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alias_auf_org.settings")
+
+application = get_wsgi_application()
